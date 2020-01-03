e89d419e6e44a1df3e734a3753b38031de6a2b6e
SJHageman <shageman@gmail.com>
2020-01-03 17:22:32 -0500

    Update model

---
+++ convert_to_gzip.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  3 13:33:32 2020
+
+@author: Tebe
+"""
+import os, sys, time
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+plt.rcParams['figure.dpi'] = 240 # fix high-dpi display scaling issues (only if you have a 4k monitor)
+
+sys.path.append(os.getcwd()) # add cwd to path
+
+from zip_codes import ZC # zip code database
+import load_file as lf # file i/o
+import myplots as mp # my plotting functions
+import file_reshaper as fr # file reshaper
+
+zc = ZC(fdir='') # initialize zip code class
+
+### define your data directory
+data_dir = r'C:\Data Science\Root Ad Data\Data\csvs'
+
+### convert files to gzip (skip if you already have gzips)
+fr.reshape_files(data_dir=data_dir) # this will take 15-20 minutes
+fr.local_hour_creator(data_dir=data_dir) # this will take another 15-20 minutes
--- model_minibatch.py
+++ model_minibatch.py
@@ -20,38 +20,61 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import SGDClassifier
 from sklearn.preprocessing import StandardScaler, RobustScaler
 from tqdm import tqdm
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, TruncatedSVD
 import matplotlib.pyplot as plt
 from joblib import dump, load
 from sklearn.metrics import recall_score
+import psutil
 
 sys.path.append(os.getcwd())
 import load_file as lf
 import model_metrics as mm
 
+# HINT
+# Set flag to limit the size of dataframe that is used based on amount of total
+# amount of RAM that is available on this machine.
+mem = psutil.virtual_memory()
+threshold = 17024618496
+if mem.total>threshold:
+    limit_size=False
+else:
+    limit_size=True
+
 #%%
 #Load all gzip data into large dataframe
-data_directory = r'D:\Root Data\csvs'
+data_directory = r'C:\Data Science\Root Ad Data\Data\csvs'
 #Load ALL of the data
 df = pd.DataFrame()
 for f in tqdm(glob.glob(os.path.join(data_directory, '*.gzip'))):
 	df = pd.concat([df,lf.temp_load(fname=f)], axis=1)
-	
-df = df.drop(['app_bundle','bid_timestamp_utc', 'tz', 'spend', 'installs'], axis=1)
+#%%
+df = df.drop(['bid_timestamp_utc', 'tz', 'spend', 'installs'], axis=1)
 df['inventory_interstitial'] = df['inventory_interstitial'].astype(int)
 df['rewarded'] = df['rewarded'].astype(int)
 df['clicks'] = df['clicks'].astype(int)
 
 #Apply mask to remove bad dates
 start_date = pd.Timestamp('2019-04-23 00:00')
-end_date = pd.Timestamp('2019-04-30 00:00')
-mask_lastweek = (df['bid_timestamp_local'] > start_date) & (df['bid_timestamp_local'] <= end_date)
+if limit_size:
+    end_date = pd.Timestamp('2019-04-30 00:00')
+else:
+    end_date = pd.Timestamp('2019-04-30 00:00')
+mask_lastweek = (df['bid_timestamp_local'] > start_date) & \
+    (df['bid_timestamp_local'] <= end_date)
 
 start_date = pd.Timestamp('2019-04-01 00:00')
-end_date = pd.Timestamp('2019-04-23 00:00')
-mask_threewks = (df['bid_timestamp_local'] > start_date) & (df['bid_timestamp_local'] <= end_date)
+if limit_size:
+    end_date = pd.Timestamp('2019-04-08 00:00')
+else:
+    end_date = pd.Timestamp('2019-04-23 00:00')
+mask_threewks = (df['bid_timestamp_local'] > start_date) & \
+    (df['bid_timestamp_local'] <= end_date)
 
 df = df.drop(['bid_timestamp_local'], axis=1)
+
+if limit_size:
+    limit_mask = np.logical_or(mask_threewks.values, mask_lastweek.values)
+    df = df[limit_mask]
 
 y = df.clicks.values
 X = df.drop(['clicks'], axis=1)
@@ -60,12 +83,17 @@ X = loo.fit_transform(X,y)
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
 
-
-X_threeweek = X[mask_threewks]
-y_threeweek = y[mask_threewks]
-X_lastweek = X[mask_lastweek]
-y_lastweek = y[mask_lastweek]
-
+if limit_size:
+    X_threeweek = X[mask_threewks[limit_mask]]
+    y_threeweek = y[mask_threewks[limit_mask]]
+    X_lastweek = X[mask_lastweek[limit_mask]]
+    y_lastweek = y[mask_lastweek[limit_mask]]
+else:
+    X_threeweek = X[mask_threewks]
+    y_threeweek = y[mask_threewks]
+    X_lastweek = X[mask_lastweek]
+    y_lastweek = y[mask_lastweek]
+#%%
 #shuffle 
 #df = df.sample(frac=1)
 #df_lastweek = df_lastweek.sample(frac=1)
@@ -94,11 +122,12 @@ y_lastweek = y[mask_lastweek]
 #print(model.score(X_test, y_test))
 #mm.model_report_card(model, X_train_res, y_train_res, X_test, y_test, normalize=False)
 #%%
-#pca = PCA(n_components = 0.9)
-#X_pca = pca.fit_transform(X_train_res)
-#print(X_pca.shape)
-##fig, ax = plt.subplots()
-##ax.scatter(X_pca[:,0],X_pca[:,1], c=y_train_res)
+pca = PCA(n_components = 2)
+X_pca = pca.fit_transform(X)
+print(X_pca.shape)
+#%%
+fig, ax = plt.subplots()
+ax.hist2d(X_pca[:,0],X_pca[:,1], bins=1000, cmap='inferno')
 #%%
 ##Let's try the simple minded way
 #y = df.clicks.values
@@ -147,55 +176,58 @@ y_lastweek = y[mask_lastweek]
 #fig, ax = plt.subplots()
 #ax.scatter(X_pca[0:1000,0],X_pca[0:1000,1], c=y_train[0:1000])
 #%%
-X_train, X_test, y_train, y_test = train_test_split(X_threeweek, y_threeweek, test_size=0.3, random_state=0)
-
-model = SGDClassifier(loss='log', penalty='elasticnet', alpha=0.001,n_jobs=-1)
-
-sm = RandomOverSampler()
-X_train_res, y_train_res = sm.fit_sample(X_train, y_train)
-model.fit(X_train_res, y_train_res)
-#%%
-y_pred =  model.predict(X_test)
-acc_score = model.score(X_test, y_test)
-print(f'The accuracy score is: {acc_score}')
-rec_score = recall_score(y_test, y_pred)
-print(f'The recall score is: {rec_score}')
-#%%
-#mm.model_report_card(model, X_train_res, y_train_res, X_test, y_test, normalize=False)
-fig, ax = plt.subplots(1, 2)
-fig.suptitle(f'accuracy score: {acc_score:.3f}, recall score: {rec_score:.3f}', fontsize=24)
-mm.plot_confusion_matrix(y_test, y_pred, ax=ax[0], normalize=True)
-mm.plot_confusion_matrix(y_test, y_pred, ax=ax[1], normalize=False)
-#%%
-y_pred =  model.predict(X_lastweek)
-acc_score = model.score(X_lastweek, y_pred)
-print(f'The accuracy score is: {acc_score}')
-rec_score = recall_score(y_lastweek, y_pred)
-print(f'The recall score is: {rec_score}')
-#%%
-#mm.model_report_card(model, X_train_res, y_train_res, X_test, y_test, normalize=False)
-fig, ax = plt.subplots(1, 2)
-fig.suptitle(f'accuracy score: {acc_score:.3f}, recall score: {rec_score:.3f}', fontsize=24)
-mm.plot_confusion_matrix(y_lastweek, y_pred, ax=ax[0], normalize=True)
-mm.plot_confusion_matrix(y_lastweek, y_pred, ax=ax[1], normalize=False)
 
 
-#%%
-pca = PCA(n_components = 2)
-X_pca = pca.fit_transform(X_train)
-sm = RandomUnderSampler()
-#%%
-X_train_underRes, y_train_underRes = sm.fit_sample(X_train, y_train)
-s = np.arange(X_train_res.shape[0])
-np.random.shuffle(s)
-X_train_underRes[s]
-y_train_underRes[s]
-X_pca_under = pca.fit_transform(X_train_underRes)
+#uncomment here
+# X_train, X_test, y_train, y_test = train_test_split(X_threeweek, y_threeweek, test_size=0.3, random_state=0)
 
-#%%
-fig, ax = plt.subplots(1,3)
-ax[0].scatter(X_pca[0:8000,0],X_pca[0:8000,1], c=y_train[0:8000], alpha=0.4, cmap='seismic')
-ax[1].scatter(X_pca_under[0:80000,0],X_pca_under[0:80000,1], c=y_train_underRes[0:80000], alpha=0.4, cmap='seismic')
+# model = SGDClassifier(loss='log', penalty='elasticnet', alpha=0.001,n_jobs=-1)
+
+# sm = RandomOverSampler()
+# X_train_res, y_train_res = sm.fit_sample(X_train, y_train)
+# model.fit(X_train_res, y_train_res)
+# #%%
+# y_pred =  model.predict(X_test)
+# acc_score = model.score(X_test, y_test)
+# print(f'The accuracy score is: {acc_score}')
+# rec_score = recall_score(y_test, y_pred)
+# print(f'The recall score is: {rec_score}')
+# #%%
+# #mm.model_report_card(model, X_train_res, y_train_res, X_test, y_test, normalize=False)
+# fig, ax = plt.subplots(1, 2)
+# fig.suptitle(f'accuracy score: {acc_score:.3f}, recall score: {rec_score:.3f}', fontsize=24)
+# mm.plot_confusion_matrix(y_test, y_pred, ax=ax[0], normalize=True)
+# mm.plot_confusion_matrix(y_test, y_pred, ax=ax[1], normalize=False)
+# #%%
+# y_pred =  model.predict(X_lastweek)
+# acc_score = model.score(X_lastweek, y_pred)
+# print(f'The accuracy score is: {acc_score}')
+# rec_score = recall_score(y_lastweek, y_pred)
+# print(f'The recall score is: {rec_score}')
+# #%%
+# #mm.model_report_card(model, X_train_res, y_train_res, X_test, y_test, normalize=False)
+# fig, ax = plt.subplots(1, 2)
+# fig.suptitle(f'accuracy score: {acc_score:.3f}, recall score: {rec_score:.3f}', fontsize=24)
+# mm.plot_confusion_matrix(y_lastweek, y_pred, ax=ax[0], normalize=True)
+# mm.plot_confusion_matrix(y_lastweek, y_pred, ax=ax[1], normalize=False)
+
+
+# #%%
+# pca = PCA(n_components = 2)
+# X_pca = pca.fit_transform(X_train)
+# sm = RandomUnderSampler()
+# #%%
+# X_train_underRes, y_train_underRes = sm.fit_sample(X_train, y_train)
+# s = np.arange(X_train_res.shape[0])
+# np.random.shuffle(s)
+# X_train_underRes[s]
+# y_train_underRes[s]
+# X_pca_under = pca.fit_transform(X_train_underRes)
+
+# #%%
+# fig, ax = plt.subplots(1,3)
+# ax[0].scatter(X_pca[0:8000,0],X_pca[0:8000,1], c=y_train[0:8000], alpha=0.4, cmap='seismic')
+# ax[1].scatter(X_pca_under[0:80000,0],X_pca_under[0:80000,1], c=y_train_underRes[0:80000], alpha=0.4, cmap='seismic')
 
 
 
